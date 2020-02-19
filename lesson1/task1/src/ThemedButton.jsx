01f1d4e677208942073df2ff4e25fe565f95ad1b
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-19 13:15:39 +0200

    add lesson1 task1

---
+++ lesson1/task1/src/ThemedButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ThemeContext } from './themes-context';
+
+class ThemedButton extends React.Component {
+  render() {
+    return (
+      <button
+        className="btn"
+        {...this.props}
+        style={{
+          background: this.context.background,
+          color: this.context.fontColor,
+        }}
+      />
+    );
+  };
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
