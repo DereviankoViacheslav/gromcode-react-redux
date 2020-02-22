bb03e1ff85b021baadde7d8a63b8b9265072e38d
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-22 11:04:45 +0200

    add lesson3 task1

--- lesson3/task1/src/counter.reducer.js
+++ lesson3/task1/src/counter.reducer.js
@@ -1,20 +1,16 @@
 import { INCREMENT, DECREMENT, RESET } from './counter.actions';
 
-const initialState = {
-  counter: 0,
-};
-
-export default function counterReducer(state = initialState, action) {
+export default function counterReducer(state = 0, action) {
   switch (action.type) {
     case INCREMENT:
       return {
         ...state,
-        counter: state.counter + 1
+        counter: state + 1
       };
     case DECREMENT:
       return {
         ...state,
-        counter: state.counter - 1
+        counter: state - 1
       };
     case RESET:
       return {
